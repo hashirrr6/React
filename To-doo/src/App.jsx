eb103de43b422a9f4fe5c1e222ffbcb7ff3a18be
hashirrr6 <hashi3904@gmail.com>
2025-01-10 19:11:03 +0530

    s

--- To-doo/src/App.jsx
+++ To-doo/src/App.jsx
@@ -1,36 +1,87 @@
 import React from "react";
-import"./App.css"
+import "./App.css";
 
-class App extends React.Component{
-  state={
-    task:"",
-    todo:[]
-  }
-  handleChange=(e)=>{
-    this.setState({task:e.target.value})
-  }
-  addTask=()=>{
-    const {task,todo}=this.state
-    this.setState({todo:[...todo,task],task:""})
-  }
-  handledelete=(ind)=>{
-    const {tudo}=this.state
-    this.setState({todo:todo.filter((item,index)=>index!=ind)})
-  }
-  render(){
-    return(
+class App extends React.Component {
+  state = {
+    task: "",
+    todo: [],
+  };
+
+  // Handle changes in the input field
+  handleChange = (e) => {
+    this.setState({ task: e.target.value });
+  };
+
+  // Add a task to the list
+  addTask = () => {
+    const { task, todo } = this.state;
+    if (task) {
+      this.setState({ todo: [...todo, { task, completed: false }], task: "" });
+    }
+  };
+
+  // Handle task deletion
+  handleDelete = (ind) => {
+    const { todo } = this.state;
+    this.setState({
+      todo: todo.filter((item, index) => index !== ind),
+    });
+  };
+
+  // Toggle task completion (checkbox click)
+  handleToggle = (ind) => {
+    const { todo } = this.state;
+    const updatedTodo = [...todo];
+    updatedTodo[ind].completed = !updatedTodo[ind].completed;
+    this.setState({ todo: updatedTodo });
+  };
+
+  // Optional: Handle task edit
+  handleEdit = (ind) => {
+    const { todo } = this.state;
+    const newTask = prompt("Edit task:", todo[ind].task);
+    if (newTask) {
+      const updatedTodo = [...todo];
+      updatedTodo[ind].task = newTask;
+      this.setState({ todo: updatedTodo });
+    }
+  };
+
+  render() {
+    return (
       <div>
         <div className="head">
-          <input onChange={this.handleChange} type="text" placeholder="Task" value={this.state.task} />
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Task"
+            value={this.state.task}
+          />
           <button onClick={this.addTask}>ADD</button>
         </div>
         <ul>
-          {
-            this.state.todo.map((el,ind)=><li key={ind}>{el} <button onClick={()=>this.handleDelete(ind)}>Delete</button></li>)
-          }
+          {this.state.todo.map((el, ind) => (
+            <li key={ind}>
+              <span
+                style={{
+                  textDecoration: el.completed ? "line-through" : "none", // Apply line-through if completed
+                }}
+              >
+                {el.task}
+              </span>
+              <input
+                type="checkbox"
+                checked={el.completed}
+                onChange={() => this.handleToggle(ind)} // Toggle completion status
+              />{" "}
+              <button onClick={() => this.handleEdit(ind)}>Edit</button>{" "}
+              <button onClick={() => this.handleDelete(ind)}>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
-}
-export default App
+}CDATASection
+
+export default App;
